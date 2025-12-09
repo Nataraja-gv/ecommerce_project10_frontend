f3c0053212de32bf375ccd04473d34531c0c0826
Nataraja-g-v <nataraja.gv@ticketsque.com>
2025-12-10 00:17:34 +0530

    commit

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,6 +1,7 @@
 import { Roboto } from "next/font/google";
 import "./globals.css";
 import { ToastContainer } from "react-toastify";
+import MainLayout from "@/component/layout/main-layout";
 
 const roboto = Roboto({
   subsets: ["latin"],
@@ -14,7 +15,13 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body suppressHydrationWarning className={`${roboto.variable} antialiased`}>{children}<ToastContainer position="top-center" autoClose={2000} /></body>
+      <body
+        suppressHydrationWarning
+        className={`${roboto.variable} antialiased`}
+      >
+        <MainLayout>{children}</MainLayout>
+        <ToastContainer position="top-center" autoClose={2000} />
+      </body>
     </html>
   );
 }
